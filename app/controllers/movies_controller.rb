0b9385a26231bace2dcbf4873f851ef8114645a7
ntuvera <ntuvera@gmail.com>
2014-07-01 19:58:36 -0400

    add migration to movies model, views/movies, edit movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,6 +6,8 @@ class MoviesController < ApplicationController
 
 #            POST   /movies(.:format)          movies#create
   def create
+    movie = Movie.create( movie_params)
+    redirect_to "/movies/#{movie.id}"
   end
 
 #  new_movie GET    /movies/new(.:format)      movies#new
@@ -15,22 +17,31 @@ class MoviesController < ApplicationController
 # edit_movie GET    /movies/:id/edit(.:format) movies#edit
 
   def edit
+    @movie = Movie.find(params[:id])
   end
 
 #      movie GET    /movies/:id(.:format)      movies#show
   def show
+    @movie = Movie.find(params[:id])
   end
 
 #            PATCH  /movies/:id(.:format)      movies#update
   def update
+    movie = Movie.find(params[:id])
+    movie.update(movie_params)
+    redirect_to "/movies/#{movie.id}"
   end
 #            PUT    /movies/:id(.:format)      movies#update
 #            DELETE /movies/:id(.:format)      movies#destroy
   def destroy
+    Movie.delete(params[:id])
+    redirect_to "/movies"
   end
 
   private
 
   def movie_params
     params.require(:movie).permit(:title, :year, :desc)
+  end
+
 end
