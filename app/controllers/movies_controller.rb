9b1cf4cd449e95bb5b0ad2cf237aaa02031dd72d
ntuvera <ntuvera@gmail.com>
2014-06-30 21:30:00 -0400

    create routes for movies model, scaffold controllers for movies and reviews

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,32 @@
+class MoviesController < ApplicationController
+#       movies GET    /movies(.:format)          movies#index
+  def index
+    @movies = Movie.all
+  end
+
+#            POST   /movies(.:format)          movies#create
+  def create
+  end
+
+#  new_movie GET    /movies/new(.:format)      movies#new
+  def new
+  end
+
+# edit_movie GET    /movies/:id/edit(.:format) movies#edit
+#      movie GET    /movies/:id(.:format)      movies#show
+  def show
+  end
+
+#            PATCH  /movies/:id(.:format)      movies#update
+  def update
+  end
+#            PUT    /movies/:id(.:format)      movies#update
+#            DELETE /movies/:id(.:format)      movies#destroy
+  def destroy
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :year, :desc)
+end
