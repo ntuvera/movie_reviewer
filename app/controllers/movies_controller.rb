0b57bf9b84e40fca0ac77b468defcd59ac06407a
ntuvera <ntuvera@gmail.com>
2014-07-03 16:32:18 -0400

    re-directs after edit and delete changed in controller.  format and style html.erbs

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,8 +7,8 @@ class MoviesController < ApplicationController
 
 #            POST   /movies(.:format)          movies#create
   def create
-    movie = Movie.create( movie_params)
-    redirect_to movie_path(movie)
+    @movie = Movie.create( movie_params)
+    redirect_to '/movies'
   end
 
 #  new_movie GET    /movies/new(.:format)      movies#new
@@ -25,19 +25,20 @@ class MoviesController < ApplicationController
 #      movie GET    /movies/:id(.:format)      movies#show
   def show
     @movie = Movie.find(params[:id])
+    @reviews = @movie.reviews
   end
 
 #            PATCH  /movies/:id(.:format)      movies#update
   def update
     movie = Movie.find(params[:id])
     movie.update(movie_params)
-    redirect_to movie_path(movie)
+    redirect_to '/movies'
   end
 #            PUT    /movies/:id(.:format)      movies#update
 #            DELETE /movies/:id(.:format)      movies#destroy
   def destroy
     Movie.delete(params[:id])
-    redirect_to movies_path
+    redirect_to '/movies'
   end
 
   private
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,20 +2,17 @@ class ReviewsController < ApplicationController
 
 #     movie_reviews GET    /movies/:movie_id/reviews(.:format)          reviews#index
   def index
-
     @movie   = Movie.find(params[:movie_id])
-    # @reviews = @movie.reviews
-    @reviews = Review.all
-    @review  = Review.new
+    @reviews = @movie.reviews
+    # @reviews = Review.all
   end
 #                   POST   /movies/:movie_id/reviews(.:format)          reviews#create
   def create
     review = Review.create(review_params)
-    redirect_to '/reviews/#{review.id}'
+    redirect_to '/movies'
   end
 #  new_movie_review GET    /movies/:movie_id/reviews/new(.:format)      reviews#new
   def new
-    @review = Review.new
     @movie  = Movie.find(params[:movie_id])
   end
 #      movie_review GET    /movies/:movie_id/reviews/:id(.:format)      reviews#show
@@ -24,25 +21,28 @@ class ReviewsController < ApplicationController
   end
 # edit_movie_review GET    /movies/:movie_id/reviews/:id/edit(.:format) reviews#edit
   def edit
+    movie = Movie.find(params[:movie_id])
     @review = Review.find(params[:id])
     @movie  = Movie.find(params[:movie_id])
   end
 #                   PATCH  /movies/:movie_id/reviews/:id(.:format)      reviews#update
 #                   PUT    /movies/:movie_id/reviews/:id(.:format)      reviews#update
   def update
+    movie = Movie.find(params[:movie_id])
     review = Review.find(params[:id])
     review.update(review_params)
-    redirect_to '/reviews#{review.id}'
+    redirect_to '/movies/#{movies.id}/reviews/#{review.id}'
   end
 #                   DELETE /movies/:movie_id/reviews/:id(.:format)      reviews#destroy
   def destroy
+    movie = Movie.find(params[:movie_id])
     Review.delete(params[:id])
-    redirect_to '/reviews/'
+    redirect_to '/movies'
   end
 
   private
   def review_params
-    params.require(:review).permit(:title, :date, :rating, :desc)
+    params.require(:review).permit(:title, :date, :rating, :desc, :movie_id)
   end
 
 end
